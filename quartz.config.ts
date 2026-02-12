c2a0d57c0053264991d3c4cf09c8035a674d411e
Xu Mintian <byl2746962022@gmail.com>
2026-02-12 13:47:22 +0800

    2026-02-12 13:47:22

--- quartz.config.ts
+++ quartz.config.ts
@@ -32,7 +32,7 @@ const config: QuartzConfig = {
           light: "#ffffff",
           lightgray: "#ffd8ea",
           gray: "#e0aeb9",
-          darkgray: "#4a4a4a",
+          darkgray: "#a23232",
           dark: "#1bbcd8",
           secondary: "#f06292",
           tertiary: "#f8bbd0",
