cab7b7058eb37924d1869513e43ab74da8540e1d
Xu Mintian <byl2746962022@gmail.com>
2026-02-12 14:23:26 +0800

    2026-02-12 14:23:26

--- quartz.config.ts
+++ quartz.config.ts
@@ -23,8 +23,8 @@ const config: QuartzConfig = {
       fontOrigin: "googleFonts",
       cdnCaching: true,
       typography: {
-        header: "Acme",
-        body: "Gideon Roman",
+        header: "Merriweather",
+        body: "Noto Serif SC",
         code: "IBM Plex Mono",
       },
       colors: {
