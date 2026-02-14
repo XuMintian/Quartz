3bfbd656268c5c10d065c984ffd4482c9444bb07
Xu Mintian <byl2746962022@gmail.com>
2026-02-15 01:45:59 +0800

    2026-02-15 01:45:59

--- quartz.layout.ts
+++ quartz.layout.ts
@@ -46,33 +46,12 @@ export const defaultContentPageLayout: PageLayout = {
     Component.Darkmode(),
     Component.MobileOnly(Component.Spacer()),
     Component.Explorer({
-              title: "📖 学习区 | Studying", 
+              title: "🗺️ 导航 | navigation", 
               folderClickBehavior: "link",
               folderDefaultState: "open",
               useSavedState: false,
-              filterFn: (node:any) => node.name !== "Attachments"&&node.name !== "Inbox"&&node.name !== "Projects"&&node.name !== "Resources"&&node.name !== "Thoughts",
-    }),
-    Component.Explorer({
-              title: "📂 项目区 | Projects", 
-              folderClickBehavior: "link",
-              folderDefaultState: "open",
-              useSavedState: false,
-              filterFn: (node:any) => node.name !== "Attachments"&&node.name !== "Inbox"&&node.name !== "Studying"&&node.name !== "Resources"&&node.name !== "Thoughts",
-    }),
-    Component.Explorer({
-              title: "🤔 一些思考 | Thoughts", 
-              folderClickBehavior: "link",
-              folderDefaultState: "open",
-              useSavedState: false,
-              filterFn: (node:any) => node.name !== "Attachments"&&node.name !== "Inbox"&&node.name !== "Studying"&&node.name !== "Resources"&&node.name !== "Projects",
-    }),
-    Component.Explorer({
-              title: "📦 资源区 | Resources", 
-              folderClickBehavior: "link",
-              folderDefaultState: "open",
-              useSavedState: false,
-              filterFn: (node:any) => node.name !== "Attachments"&&node.name !== "Inbox"&&node.name !== "Studying"&&node.name !== "Projects"&&node.name !== "Thoughts",
-    }),
+              filterFn: (node:any) => node.name !== "Attachments"&&node.name !== "Inbox",
+})
   ],
   right: [
     
