36c73779487326ab4063d2845b60428bb8056290
Xu Mintian <byl2746962022@gmail.com>
2026-02-15 00:13:23 +0800

    2026-02-15 00:13:23

--- quartz.config.ts
+++ quartz.config.ts
@@ -19,6 +19,7 @@ const config: QuartzConfig = {
     baseUrl: "quartz.jzhao.xyz",
     ignorePatterns: ["private", "templates", ".obsidian","Inbox"],
     defaultDateType: "modified",
+    
     theme: {
       fontOrigin: "googleFonts",
       cdnCaching: true,
