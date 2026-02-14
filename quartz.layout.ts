3013f8b1c7c69cacffeb7867c76eaa92103d3dba
Xu Mintian <byl2746962022@gmail.com>
2026-02-15 00:33:54 +0800

    2026-02-15 00:33:54

--- quartz.layout.ts
+++ quartz.layout.ts
@@ -46,7 +46,9 @@ export const defaultContentPageLayout: PageLayout = {
     Component.Darkmode(),
     Component.MobileOnly(Component.Spacer()),
     Component.Explorer({
-              title: "我的知识库", // 👈 在这里改！想叫什么叫什么，比如 "Knowledge" 或 "Recent Writing"
+              title: "我的知识库", 
+              folderDefaultState: "open",
+              useSavedState: false,
               filterFn: (node:any) => node.name !== "Attachments"&&node.name !== "Inbox",
     }),
     
