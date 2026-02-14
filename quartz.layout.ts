e15a5d29f97e9a00fd474333a162eebc8b5a0788
Xu Mintian <byl2746962022@gmail.com>
2026-02-14 13:47:36 +0800

    Quartz sync: Feb 14, 2026, 1:47 PM

--- quartz.layout.ts
+++ quartz.layout.ts
@@ -45,8 +45,9 @@ export const defaultContentPageLayout: PageLayout = {
     //}),
     Component.MobileOnly(Component.Spacer()),
     Component.Explorer({
-  title: "我的知识库", // 👈 在这里改！想叫什么叫什么，比如 "Knowledge" 或 "Recent Writing"
-}),
+              title: "我的知识库", // 👈 在这里改！想叫什么叫什么，比如 "Knowledge" 或 "Recent Writing"
+              filterFn: (node:any) => node.name !== "Attachments",
+    }),
     
   ],
   right: [
